61cc0d79d45d484d3b566ff44f9225d7e89c27ee
mifthas67 <ag.muhdmifthas@gmail.com>
2024-05-31 20:23:02 +0300

    modified script

--- script.js
+++ script.js
@@ -3,3 +3,17 @@ kfkosajgp
 
 fkgjopgjkpokgpok
 gfsd\g][gldp[fgk
+
+    flg[pkhkr[pkhkr
+    ]]
+
+
+    kgdpoghkphokhk[kgdpoghkphokhk
+
+        lg[;sdklp[klghp[lk[kl[length
+
+            dkgspokgkgl.'.;
+            
+            gkdsh[pkl';h,l;'.'
+        ]]]]]
+    ]
